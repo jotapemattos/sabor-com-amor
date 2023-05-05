9228cb26fa66ae877ad8632956498e44d0443f26
Joao Pedro <jpmrc49@gmail.com>
2023-05-05 18:50:27 -0300

    build the navBar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,3 +1,22 @@
+import Link from 'next/link';
+
 export function NavBar() {
-  return <nav></nav>;
+  return (
+    <nav className="w-screen h-20 fixed inset-0 bg-transparent backdrop-blur-lg flex items-center justify-around">
+      <img
+        src="/logo.svg"
+        alt="logo-image"
+        className="w-16 h-16 rounded-full"
+      />
+      <span className="flex gap-20 items-center">
+        <Link href={'/'}>
+          <p>Sobre</p>
+        </Link>
+        <Link href={'/produtos'}>
+          <p>Produtos</p>
+        </Link>
+        <p>Encomendas</p>
+      </span>
+    </nav>
+  );
 }
