ef66c2c5d58fa32459d647b7da0766933385562e
Joao Pedro <jpmrc49@gmail.com>
2023-05-06 17:27:24 -0300

    build the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+import MainContent from '@/components/Home/MainContentComponent';
+import ProductsShowCase from '@/components/Home/ProductsShowCaseComponent';
+
 export default function Home() {
-  return <main className="bg-slate-600 min-h-screen min-w-screen"></main>;
+  return (
+    <div className="min-h-screen min-w-screen flex pt-20 items-center justify-around">
+      <MainContent />
+      <ProductsShowCase />
+    </div>
+  );
 }
