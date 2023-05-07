ff4e221cbadab66b766bfc9b171141737ce84cbb
Joao Pedro <jpmrc49@gmail.com>
2023-05-07 20:48:10 -0300

    change the quantity description

--- src/data/data.ts
+++ src/data/data.ts
@@ -1,15 +1,15 @@
 import brownie from '../assets/brownie.svg';
-import cookiesIntegral from '../assets/cookies-integral.svg';
+import cookiesIntegral from '../assets/cookies-integral 1.svg';
 import cookies from '../assets/cookies.svg';
-import enroladinhoGoiaba from '../assets/enroladinho-de-goiaba-vidro.svg';
-import paoCaseiroCalabresa from '../assets/pao-caseiro-com-calabresa.svg';
-import paoCaseiroMussarela from '../assets/pao-caseiro-com-mussarela.svg';
-import paoCaseiro from '../assets/pao-caseiro-italiano.svg';
-import paoDeForma from '../assets/pao-forma-caseiro.svg';
-import paoDeFormaSemiIntegral from '../assets/pao-forma-semi-integral.svg';
-import waffle from '../assets/waffle-2-discos.svg';
+import enroladinhoGoiaba from '../assets/enroladinho-de-goiaba-vidro 1.svg';
+import paoCaseiroCalabresa from '../assets/pao-caseiro-com-calabresa 1.svg';
+import paoCaseiroMussarela from '../assets/pao-caseiro-com-mussarela 1.svg';
+import paoCaseiro from '../assets/pao-caseiro-italiano 1.svg';
+import paoDeForma from '../assets/pao-forma-caseiro 1.svg';
+import paoDeFormaSemiIntegral from '../assets/pao-forma-semi-integral 1.svg';
+import waffle from '../assets/waffle-2-discos 1.svg';
 
-type DataProps = {
+export type DataProps = {
   name: string;
   price: string;
   image: string;
@@ -78,14 +78,14 @@ export const data: DataProps[] = [
     name: 'Cookie com gotas de chocolate meio amargo',
     price: '12,00',
     image: cookies,
-    quantity: 'Pacote 200g',
+    quantity: '200g',
     description: ''
   },
   {
     name: 'Cookie de banana com gotas de chocolate meio amargo.',
     price: '14,00',
     image: cookiesIntegral,
-    quantity: 'Pacote 200g',
+    quantity: '200g',
     description: 'Sem trigo e sem açúcar'
   }
 ];
