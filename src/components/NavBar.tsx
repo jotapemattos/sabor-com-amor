3794662cb2e67b8a02a363b4e882a1c10a24d37d
Joao Pedro <jpmrc49@gmail.com>
2023-05-07 20:47:17 -0300

    add a hover animation in each category

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,20 +2,28 @@ import Link from 'next/link';
 
 export function NavBar() {
   return (
-    <nav className="w-screen h-20 fixed inset-0 bg-transparent backdrop-blur-lg flex items-center justify-around">
+    <nav className="w-screen h-24 fixed z-20 inset-0 bg-transparent backdrop-blur-lg flex items-center justify-around shadow-xl">
       <img
         src="/logo.svg"
         alt="logo-image"
         className="w-16 h-16 rounded-full"
       />
-      <span className="flex gap-20 items-center">
+      <span className="flex gap-20 items-center text-white font-bold">
         <Link href={'/'}>
-          <p>Sobre</p>
+          <p className="hover:text-[#694A43] transition-colors duration-300">
+            Sobre
+          </p>
         </Link>
         <Link href={'/produtos'}>
-          <p>Produtos</p>
+          <p className="hover:text-[#694A43] transition-colors duration-300">
+            Produtos
+          </p>
         </Link>
-        <p>Encomendas</p>
+        <Link href={'/encomendas'}>
+          <p className="hover:text-[#694A43] transition-colors duration-300">
+            Encomendas
+          </p>
+        </Link>
       </span>
     </nav>
   );
