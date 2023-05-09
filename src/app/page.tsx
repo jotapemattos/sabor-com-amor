3e8a3cf835d435fd9755d2841a363461aea80ab8
Joao Pedro <jpmrc49@gmail.com>
2023-05-09 15:56:47 -0300

    add responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import ProductsShowCase from '@/components/Home/ProductsShowCaseComponent';
 
 export default function Home() {
   return (
-    <div className="min-h-screen min-w-screen flex pt-20 items-center justify-around">
+    <div className="min-h-screen min-w-screen flex flex-col lg:flex-row pt-28 xl:pt-32 2xl:pt-20 xl:pb-10 2xl:pb-0 items-center justify-around gap-14 xl:gap-0">
       <MainContent />
       <ProductsShowCase />
     </div>
