7bf3d760096085fd4423bd816a6f0b863b134467
Joao Pedro <jpmrc49@gmail.com>
2023-05-11 22:52:17 -0300

    add responsiveness

--- src/app/produtos/page.tsx
+++ src/app/produtos/page.tsx
@@ -11,7 +11,7 @@ const Page = () => {
 
   return (
     <div className="flex items-center justify-center">
-      <div className="w-4/5 grid grid-cols-4 place-items-center gap-20 pt-32 pb-20 relative z-0">
+      <div className="w-4/5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 place-items-center gap-20 pt-32 pb-20 relative z-0">
         {products.map((product, i) => (
           <ProductCards key={product.name} product={product} index={i} />
         ))}
