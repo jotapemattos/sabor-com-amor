eb80175b5aeba7cb7eb72208e3f1cb6f1f6c5cb0
João Pedro <jpmrc49@gmail.com>
2023-08-31 09:52:15 -0300

    style: change the border radius to a smaller one

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Button = ({ children }: { children: React.ReactNode }) => {
   return (
-    <button className="bg-[#cea17c] rounded-2xl px-10 py-4 text-[#081930] font-bold hover:bg-[#644f3d] transition-all duration-300">
+    <button className="bg-[#cea17c] rounded-lg px-10 py-4 text-[#081930] font-bold hover:bg-[#644f3d] transition-all duration-300">
       {children}
     </button>
   );
