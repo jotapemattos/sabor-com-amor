2a99986e6e102c665dd591c3ea1a1ab51cf7f29d
João Pedro <jpmrc49@gmail.com>
2023-10-31 16:24:57 -0300

    feat: adds a home route to logo

--- src/components/Header/NavBar.tsx
+++ src/components/Header/NavBar.tsx
@@ -7,11 +7,13 @@ import HamburgerIcon from './HamburguerIcon';
 export function NavBar() {
   return (
     <nav className="header w-screen h-24 fixed z-20 inset-0 flex items-center justify-between lg:justify-around p-4 lg:p-0 shadow-xl">
-      <img
-        src="/logo.svg"
-        alt="logo-image"
-        className="w-16 h-16 rounded-full"
-      />
+      <Link href={'/'}>
+        <img
+          src="/logo.svg"
+          alt="logo-image"
+          className="w-16 h-16 rounded-full"
+        />
+      </Link>
       <span className="hidden lg:flex gap-20 items-center text-white font-bold">
         <Link href={'/'}>
           <p className="hover:text-[#cea17c] transition-colors duration-300">
