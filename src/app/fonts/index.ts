04e4797a3f9fc2747fd93d9890e83bbae1ef9c30
João Pedro <jpmrc49@gmail.com>
2024-05-07 20:55:34 -0300

    feat: adds a new font

--- src/app/fonts/index.ts
+++ src/app/fonts/index.ts
@@ -1,9 +1,9 @@
-import { Inter } from 'next/font/google';
-import localFont from 'next/font/local';
+import { Inter } from 'next/font/google'
+import localFont from 'next/font/local'
 
 export const telma = localFont({
   src: './Telma-Regular.otf',
   variable: '--font-telma'
-});
+})
 
-export const inter = Inter({ subsets: ['latin'] });
+export const inter = Inter({ subsets: ['latin'] })
