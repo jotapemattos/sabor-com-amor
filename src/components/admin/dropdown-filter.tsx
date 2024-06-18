7f739422cf16415a5e3475450b54c104f604c5bb
João Pedro <jpmrc49@gmail.com>
2024-06-18 12:07:14 -0300

    chore:: installs dropdown component

---
+++ src/components/admin/dropdown-filter.tsx
@@ -0,0 +1,40 @@
+'use client'
+import { useState } from 'react'
+
+import { Button } from '../ui/button'
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger
+} from '../ui/dropdown-menu'
+
+import { ArrowDownWideNarrow } from 'lucide-react'
+
+export function DropdownFilter() {
+  const [ascending, setAscending] = useState(false)
+  return (
+    <>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="outline" size="sm" className="h-8 gap-1 text-sm">
+            <ArrowDownWideNarrow className="size-4" />
+            <span>Filtrar</span>
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <DropdownMenuLabel>Filtrar por</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuCheckboxItem checked={ascending} onSelect={() => setAscending(true)}>
+            Arquivado
+          </DropdownMenuCheckboxItem>
+          <DropdownMenuCheckboxItem checked={!ascending} onSelect={() => setAscending(false)}>
+            Ativados
+          </DropdownMenuCheckboxItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </>
+  )
+}
