8f447bbe53b958704beed6f318dfa1d0b81448f3
João Pedro <jpmrc49@gmail.com>
2024-07-02 16:16:13 -0300

    chore: removes unused images

--- src/app/actions/get-products.ts
+++
@@ -1,9 +0,0 @@
-import { supabase } from '@/supabase/supabaseComponent'
-
-export async function getProducts() {
-  const { data, error } = await supabase.from('products').select()
-  if (error) {
-    throw new Error('Produtos nao encontrados')
-  }
-  return data
-}
