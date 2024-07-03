10f65e06adaee8e64689278d4e0fdb542a8fe848
João Pedro <jpmrc49@gmail.com>
2024-07-02 23:09:38 -0300

    fix: build problems

--- src/app/(admin)/(admin)/admin/page.tsx
+++ src/app/(admin)/(admin)/admin/page.tsx
@@ -1,12 +1,3 @@
-import { redirect } from 'next/navigation'
-
-import { createClient } from '@/supabase/serverComponent'
-
 export default async function Admin() {
-  const supabase = createClient()
-  const {
-    data: { user }
-  } = await supabase.auth.getUser()
-
   return <div>Admin</div>
 }
